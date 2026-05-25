3715a17516b0a526d8c1aa0b8aaa11eeb3ca17b2
M1RVKL <bielov.vlad@gmail.com>
2026-05-25 16:55:58 +0300

    fixes to runner according to linters

--- runner-setup.sh
+++ runner-setup.sh
@@ -1,19 +1,17 @@
-echo "Оновлення системи..."
+echo "System updating"
 sudo apt-get update && sudo apt-get upgrade -y
 
-echo "Встановлення базових утиліт (curl, tar, ssh)..."
+echo "Installing curl, tar, etc..."
 sudo apt-get install -y curl tar build-essential openssh-client
 
-echo "Створення робочої директорії..."
-mkdir -p ~/actions-runner && cd ~/actions-runner
+mkdir -p ~/actions-runner
+cd ~/actions-runner || exit 1
 
-echo "Завантаження GitHub Actions Runner..."
 curl -o actions-runner-linux-x64-2.316.1.tar.gz -L https://github.com/actions/runner/releases/download/v2.316.1/actions-runner-linux-x64-2.316.1.tar.gz
 
-echo "Розпакування архіву..."
 tar xzf ./actions-runner-linux-x64-2.316.1.tar.gz
 
-echo "Встановлення залежностей раннера..."
+echo "Installing dependencies..."
 sudo ./bin/installdependencies.sh
 
-echo "Базова підготовка завершена! Готово до ручної конфігурації."
+echo "Everything is ready."
